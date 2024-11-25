2d46d920b9ba118859e3db3e6afffd870eeea934
Sensei869 <rajeshmaharjan869.com>
2024-11-25 07:13:56 +0545

    login signup page done

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-// script.js
-const toggleButton = document.querySelector('.toggle_btn');
-const navLinks = document.querySelector('.navlinks');
-const navButton = document.querySelector('.navbutton');
+const form = document.getElementById('forms')
+const firstname_input = document.getElementById('firstname-input')
+const email_input = document.getElementById('email-input')
+const password_input = document.getElementById('password-input')
+const confirmpassword_input = document.getElementById('confirmpassword-input')
 
-toggleButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-  navButton.classList.toggle('active');
-});
+form.addEventListener('submit', (e) => {
+  // e.preventDefault() prevent submit
+
+  let errors = []
+
+  if(firstname_input){
+    errors = getSignupFormErrors(firstname_input.ariaValueMax. email_input.value, password_input.value, confirmpassword_input.value)
+  }
+  else{
+
+    errors = getLoginFormErrors(email_input.value, password_input.value)
+  }
+  
+})
